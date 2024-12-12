07d28c3e3e222b358a4b02076ce7f7b1b8822c1c
GitHub-Faye <a1092246441@gmail.com>
2024-12-12 16:48:37 +0800

    clear

--- music_controller/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from . import models
-
-class RoomSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Room
-        fields = ('id', 'code', 'host', 'guest_can_pass', 'votes_to_skip','created_at')
-        
--- music_controller/frontend/src/components/CreateRoomPage.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function CreateRoomPage() {
-  return (
-    <h1>this is  CreateRoomPage</h1>
-  );
-}
